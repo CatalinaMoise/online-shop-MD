0f8091d78ddb69736c8f680375987eabf18c32fd
Catalina <catalina.moise96@yahoo.com>
2023-03-20 20:40:28 +0200

    initial commit

---
+++ src/main/java/com/sda/OnlineShopMD/OnlineShopMdApplication.java
@@ -0,0 +1,13 @@
+package com.sda.OnlineShopMD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShopMdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineShopMdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sda/OnlineShopMD/OnlineShopMdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sda.OnlineShopMD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShopMdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
