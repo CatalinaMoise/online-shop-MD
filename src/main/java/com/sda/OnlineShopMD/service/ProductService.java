2636b044fc13758e05d7abcbf5394e5e33695d4b
Catalina <catalina.moise96@yahoo.com>
2023-03-23 21:07:14 +0200

    implemented home page

--- src/main/java/com/sda/OnlineShopMD/service/ProductService.java
+++ src/main/java/com/sda/OnlineShopMD/service/ProductService.java
@@ -8,6 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
 public class ProductService {
     @Autowired
@@ -18,6 +21,17 @@ public class ProductService {
     public void create(ProductDto productDto){
         Product product = productMapper.map(productDto); //echivalent operatiunii int x = 1+2
         productRepository.save(product); //product va intra in baza de date
+
+    }
+
+    public List<ProductDto> getAllProductDtoList() {
+        List<ProductDto> productDtoList = new ArrayList<>();
+        List<Product> productList = productRepository.findAll(); //rezultatul lui find all, il pun intr-o variabila de tip list<product>:
+        for (Product product : productList) {
+            ProductDto productDto = productMapper.map(product);
+            productDtoList.add(productDto);
+        }
+            return productDtoList;
 
     }
 }
